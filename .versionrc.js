fecb8a219d115b67b037cfd954761811b7e4670d
Jasim Khan <jasim.khan@allmyhomes.com>
2021-01-18 12:34:45 +0100

    chore(standard-version): add types for documentation

---
+++ .versionrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", hidden: false },
+    { type: "docs", hidden: false },
+    { type: "style", section: "Code Styles", hidden: true },
+    { type: "refactor", section: "Refactors", hidden: true },
+    { type: "perf", section: "Performance Improvements", hidden: true },
+    { type: "test", section: "Tests", hidden: true },
+    { type: "improvement", section: "Improvements", hidden: true },
+  ],
+};
